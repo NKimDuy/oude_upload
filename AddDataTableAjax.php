054a198d79c4339765d52c171e762432617141e5
unknown <Administrator@duynk-cfde.dttx>
2020-07-31 16:09:56 +0700

    commit lan thu n

--- AddDataTableAjax.php
+++ AddDataTableAjax.php
@@ -2,7 +2,7 @@
 	require_once("./connectDB.php");
 	$con = Connect();
 	$tableName = $_POST['table_name'];
-	$data = $_POST['data'];
+	$data = $_POST['row'];
 	$sql = "INSERT INTO " . $tableName . " VALUES(";
 	for ($i = 0 ; $i < count($data) ; $i++)
 	{
